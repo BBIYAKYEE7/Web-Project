ed4a8480ef58b3cd1717e9a73f29569203e8d482
BBIYAKYEE7 <dadalgojr@gmail.com>
2024-03-23 13:58:09 +0900

    Code Update

--- src/components/map/GoogleMapProvider.tsx
+++ src/components/map/GoogleMapProvider.tsx
@@ -1,11 +1,10 @@
 import {Wrapper} from "@googlemaps/react-wrapper"
 
 export default function GoogleMapProvider({children}: {children: React.ReactNode}) {
-    const key = AIzaSyB50bhEzzTuFo7p4VpWE_GpJgSkDLy3GIA;
+    const key = import.meta.env.VITE_SECOND_GOOGLE_API_KEY;
     return (
-        <Wrapper apiKey={AIzaSyB50bhEzzTuFo7p4VpWE_GpJgSkDLy3GIA}>
+        <Wrapper apiKey={key}>
             {children}
         </Wrapper>
     );
 }
-
